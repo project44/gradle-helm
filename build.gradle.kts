d866e4d96040170d34528d2eae34c9c79259d9a7
Brandon Evers <beversproject44.com>
2020-12-22 17:23:57 -0600

    it publishes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,33 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import groovy.lang.GroovyObject
 import org.junit.platform.gradle.plugin.JUnitPlatformPlugin
 import java.time.format.DateTimeFormatter
 import java.time.LocalDateTime
+import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
+import org.jfrog.gradle.plugin.artifactory.dsl.ResolverConfig
 
 group = "ca.cutterslade.gradle"
 version = "1.0.0-beta-8-p44"
 
 repositories {
   jcenter()
+  maven("http://artifactory.internal-p-44.com/artifactory/plugins-release")
 }
 
 buildscript {
   dependencies {
     classpath("org.junit.platform:junit-platform-gradle-plugin:1.1.0")
+    classpath("org.jfrog.buildinfo:build-info-extractor-gradle:latest.release")
   }
 }
-
 plugins {
   `java-gradle-plugin`
   `kotlin-dsl`
   `junit-test-suite`
   kotlin("jvm") version "1.2.61"
   id("com.gradle.plugin-publish") version "0.10.0"
+  id("com.jfrog.artifactory") version "4.18.2"
+id("maven-publish")
 }
 
 sourceSets.create("functionalTest") {
@@ -52,33 +58,59 @@ dependencies {
   add("functionalTestRuntime", "org.junit.platform:junit-platform-engine:1.3.1")
   add("functionalTestRuntime", "org.jetbrains.spek:spek-junit-platform-engine:1.1.5")
 }
-
-gradlePlugin {
-  plugins {
-    create("helm") {
-      id = "ca.cutterslade.helm"
-      implementationClass = "ca.cutterslade.gradle.helm.HelmPlugin"
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            /*artifact(sourcesJar.get())*/
+        }
     }
-  }
-  testSourceSets(sourceSets["functionalTest"])
+}
+artifactory {
+    setContextUrl("http://artifactory.internal-p-44.com/artifactory/")
+    publish(delegateClosureOf<PublisherConfig> {
+        setProperty("repoKey", "plugins-release-local")
+        repository(delegateClosureOf<GroovyObject> {
+            setProperty("username", "admin")
+            setProperty("password", "admin")
+        })
+        defaults(delegateClosureOf<GroovyObject> {
+            invokeMethod("publications", "mavenJava")
+            setProperty("publishPom", true)
+            setProperty("publishArtifacts", true)
+        })
+    })
+    resolve(delegateClosureOf<ResolverConfig> {
+        setProperty("repoKey", "libs-release")
+    })
 }
 
-pluginBundle {
-  website = "https://github.com/wfhartford/gradle-helm"
-  vcsUrl = "https://github.com/wfhartford/gradle-helm.git"
-  tags = listOf("helm")
-  description = "Plugin supporting basic helm commands for a gradle build."
+/*gradlePlugin {*/
+  /*plugins {*/
+    /*create("helm") {*/
+      /*id = "ca.cutterslade.helm"*/
+      /*implementationClass = "ca.cutterslade.gradle.helm.HelmPlugin"*/
+    /*}*/
+  /*}*/
+  /*testSourceSets(sourceSets["functionalTest"])*/
+/*}*/
 
-  (plugins) {
-    "helm" {
-      id = "ca.cutterslade.helm"
-      displayName = "Gradle Helm Plugin"
-      version = project.version.toString().replace("-SNAPSHOT", "-${DateTimeFormatter.ofPattern("uuuuMMddHHmmss").format(LocalDateTime.now())}")
-    }
-  }
-}
+/*pluginBundle {*/
+  /*website = "https://github.com/wfhartford/gradle-helm"*/
+  /*vcsUrl = "https://github.com/wfhartford/gradle-helm.git"*/
+  /*tags = listOf("helm")*/
+  /*description = "Plugin supporting basic helm commands for a gradle build."*/
 
-tasks["publishPlugins"].dependsOn(tasks["check"])
+  /*(plugins) {*/
+    /*"helm" {*/
+      /*id = "ca.cutterslade.helm"*/
+      /*displayName = "Gradle Helm Plugin"*/
+      /*version = project.version.toString().replace("-SNAPSHOT", "-${DateTimeFormatter.ofPattern("uuuuMMddHHmmss").format(LocalDateTime.now())}")*/
+    /*}*/
+  /*}*/
+/*}*/
+
+/*tasks["publishPlugins"].dependsOn(tasks["check"])*/
 
 tasks {
   create<Test>("functionalTest") {
